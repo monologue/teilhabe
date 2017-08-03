38ed17c40fffb046389a565fc0bf0cd58074c2e0
monologue <reg.taube@gmail.com>
2017-08-03 17:59:13 +0200

    add project

---
+++ src/test/java/teilhabe/teilhabe/TeilhabeApplicationTests.java
@@ -0,0 +1,16 @@
+package teilhabe.teilhabe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeilhabeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
